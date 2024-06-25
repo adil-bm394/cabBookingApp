31fc8b0cfcbae5f5b35456dd78a226bbdf93ce4d
adil-bm394 <adil@binmile.com>
2024-06-25 16:19:07 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ import Confirmation from './component/Confirmation';
 import { PersonalInfoFormData, ServicesDetailsFormData, BookingDetailsFormData } from './types'; // Import interfaces
 
 const App: React.FC = () => {
-  const [formData, setFormData] = useState<any>({});
+  const [formData, setFormData] = useState<{
+    personalInfo?: PersonalInfoFormData;
+    servicesDetails?: ServicesDetailsFormData;
+    bookingDetails?: BookingDetailsFormData;
+  }>({});
 
   useEffect(() => {
-   
     const storedFormData = JSON.parse(localStorage.getItem('formData') || '{}');
     if (Object.keys(storedFormData).length > 0) {
       setFormData(storedFormData);
@@ -30,7 +33,6 @@ const App: React.FC = () => {
   };
 
   useEffect(() => {
-    // Store form data in localStorage whenever formData changes
     localStorage.setItem('formData', JSON.stringify(formData));
   }, [formData]);
 
@@ -41,11 +43,11 @@ const App: React.FC = () => {
         <Route path="/services" element={<ServicesDetails onNext={handleServicesDetailsSubmit} />} />
         <Route path="/booking" element={<BookingDetails onBook={handleBookingDetailsSubmit} />} />
         <Route path="/confirmation" element={
-          Object.keys(formData).length > 0 ? (
+          formData.personalInfo && formData.bookingDetails ? (
             <Confirmation
-              name={formData.personalInfo?.name}
-              bookingDate={formData.bookingDetails?.appointmentDate}
-              bookingTime={formData.bookingDetails?.appointmentTime}
+              name={formData.personalInfo.name}
+              bookingDate={formData.bookingDetails.appointmentDate}
+              bookingTime={formData.bookingDetails.appointmentTime}
             />
           ) : (
             <Navigate to="/" />
